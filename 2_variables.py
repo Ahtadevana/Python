75f5ef147a2fb036bb33fd9f3f6433aa6b095151
xervii <ahtaullahakmali@gmail.com>
2026-01-07 18:52:41 +0700

    Variables and output

---
+++ 2_variables.py
@@ -0,0 +1,25 @@
+#variables
+
+#string
+name = "Xervii"
+password = "Hellyeah123"
+
+print(f"namamu {name}, password: {password}")
+
+#integer
+age = 18
+
+print(f"umurmu: {age}")
+
+#float
+gpa = 3.41
+
+print(f"IPKmu: {gpa}")
+
+#boolean
+is_online = True
+
+if is_online:
+    print(f"{name} online!")
+else:
+    print(f"{name} offile!")
