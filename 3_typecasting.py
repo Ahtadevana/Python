1700441d1af3cc6de14c429c707f23051b8b1efc
xervii <ahtaullahakmali@gmail.com>
2026-01-09 17:25:45 +0700

    Adding codes

---
+++ 3_typecasting.py
@@ -0,0 +1,21 @@
+#Typecasting -> Turning a variable type for a different type (ex. float -> int, int -> str, etc)
+
+#Integer -> String
+age = 10
+age = str(age)
+
+#Float -> integer
+gpa = 3.41
+gpa = int(gpa)
+
+#Accepting user input
+name = bool(input("Input your name: "))
+
+#Checking whether variable 'name' is already "Filled"
+if name == False:
+    print("You didn't input your name!")
+else:
+    print("Acces granted: ")
+    print(type(age))
+    print(type(gpa))    
+    print(type(name))
