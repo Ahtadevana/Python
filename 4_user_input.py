13743ecfa7b4e75e74fa930df57e280e10928264
xervii <ahtaullahakmali@gmail.com>
2026-01-11 14:44:13 +0700

    Adding cube volume calculator function

--- 4_user_input.py
+++ 4_user_input.py
@@ -1,4 +1,4 @@
-#accepting user input(Project ex. Madlibs, shopping cart)
+#accepting user input(Project ex. Madlibs, volume calculator,shopping cart)
 
 def madlibs_game():
     mood = input("How are you feeling right now?: ")
@@ -7,6 +7,11 @@ def madlibs_game():
 
     print(f"You're feeling {mood}")
     print(f"You're currently at {place} then accidentally bump into {thing}")
+
+def cube_volume():
+    edge = float(input("What's the length of the cube's (any) edge?: "))
+    total = pow(edge, 3)
+    print(f"The volume of your cube would be: {total:.0f}cm^3")
 
 def shopping_cart():
     item = input("What do you want to buy right now?: ")
@@ -17,4 +22,4 @@ def shopping_cart():
     print(f"You're currently buying {quantity} {item}(s)")
     print(f"Your total would be ${total}")
 
-shopping_cart()
+cube_volume()
