a63a7e92121147c88ff089e89e9152ae5a3ca53f
xervii <ahtaullahakmali@gmail.com>
2026-01-16 14:17:18 +0700

    Adding further definitions

--- projectF_countdownclock.py
+++ projectF_countdownclock.py
@@ -1,14 +1,14 @@
 #making some sort of digital clock with time module
-import time
+import time    #a module
 x = int(input("Input seconds: "))
 
 for i in range(x):
-    time.sleep(1)
+    time.sleep(1)   # time.sleep from time module (pauses for 1 second)
     seconds = x % 60
     minutes = (x // 60) % 60
     hours = (x // 3600) % 24
     days = x // 86400
 
-    print(f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}", end="\r")
-    x -= 1
+    print(f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}", end="\r")  #\r overwrites the previous output
+    x -= 1    # -1 seconds per iteration
 print("time's up!")
